d41e978fa1c3859e2a519e5f61cb5e53224cdb06
joechea <s.chea@aupp.edu.kh>
2024-06-14 14:36:25 +0700

    update validation lesson code

--- 22_blog-chef/routes/home/home.js
+++ 22_blog-chef/routes/home/home.js
@@ -1,4 +1,7 @@
-import { join } from "path";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default (req, res) =>
   res.sendFile(join(__dirname, "../../" + "public", "client", "index.html"));
